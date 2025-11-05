5058bf1771f800af0e35a6a8dfd7b0ccd85ac029
Only-Abhiram <abhi1@AbhiChikki.(none)>
2025-11-05 12:50:26 +0530

    new updtae

--- src/components/socials/items.jsx
+++ src/components/socials/items.jsx
@@ -6,5 +6,6 @@ const items = [
   { icon: <img src="https://img.icons8.com/?size=100&id=0wr4dYtBFwHA&format=png&color=FFFFFF" alt="" />, color: 'deeppink', label: '', link:'https://www.instagram.com/not.abhiram_/' },
   { icon: <img src="https://img.icons8.com/?size=100&id=34349&format=png&color=FFFFFF" alt="" />, color: 'blue', label: '' , link:'https://www.linkedin.com/in/abhiram-palle-bb8b10285/'},
   { icon: <img src="https://img.icons8.com/?size=100&id=fJp7hepMryiw&format=png&color=FFFFFF" alt="" />, color: 'dimgray', label: '' , link:'https://x.com/abhiram_0'},
+  { icon: <img src="https://www.pngall.com/wp-content/uploads/13/Github-Logo.png" alt="" />, color: 'white', label: '' , link:'https://github.com/only-Abhiram/'},
 ];
 export default items;
--- src/components/socials/socials.jsx
+++ src/components/socials/socials.jsx
@@ -46,7 +46,7 @@ const gradientMapping = {
                 boxShadow: "0 0 0 0.1em hsla(0, 0%, 100%, 0.3) inset",
               }}
             >
-              <span className="m-auto w-[1.5em] h-[1.5em] flex items-center justify-center" aria-hidden="true">
+              <span className="m-auto w-[1.7em] h-[1.7em] flex items-center justify-center" aria-hidden="true">
                 {item.icon}
               </span>
             </span>
